a6f37f32b9b6d895ad952cfb5a4fe4ec951ba784
StephaneBg <stephane@baiget.fr>
2014-08-04 12:47:50 +0200

    Changed theme.

--- ScoreIt/src/main/java/com/sbgapps/scoreit/BaseActivity.java
+++ ScoreIt/src/main/java/com/sbgapps/scoreit/BaseActivity.java
@@ -31,7 +31,6 @@ import com.readystatesoftware.systembartint.SystemBarTintManager;
 public class BaseActivity extends Activity {
 
     public void setAccentDecor() {
-        getWindow().getDecorView().setBackgroundResource(R.drawable.background_pattern);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             Window win = getWindow();
             WindowManager.LayoutParams winParams = win.getAttributes();
